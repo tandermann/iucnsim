19f0df72e0f1ca0496962f57eee427d4c38645ae
Tobias Andermann <tobias.andermann@bioenv.gu.se>
2021-02-04 18:39:00 +0100

    install all functions

--- R/iucnsim_functions.R
+++ R/iucnsim_functions.R
@@ -1,3 +1,50 @@
+#' iucnsim functions
+#' @export
+#' @import reticulate
+#' @import rredlist
+#'
+reticulate::source_python("https://raw.githubusercontent.com/tobiashofmann88/iucn_extinction_simulator/master/iucn_sim/iucn_sim.py")
+
+
+#' Estimate status transition rates
+#' @export
+#' @import reticulate
+
+estimate_transition_rates = function(extant_taxa_current_status,
+                                     iucn_history_file,
+                                     outdir,
+                                     possibly_extinct_list,
+                                     extinction_probs_mode=0,
+                                     species_specific_regression=FALSE,
+                                     rate_samples=100,
+                                     n_gen=100000,
+                                     burnin=1000,
+                                     seed=NULL,
+                                     load_from_file=FALSE,
+                                     return_object=TRUE
+){
+
+  # source python function
+  reticulate::source_python("https://raw.githubusercontent.com/tobiashofmann88/iucn_extinction_simulator/master/iucn_sim/iucn_sim.py")
+  transition_rates_obj = transition_rates(extant_taxa_current_status,
+                                          iucn_history_file,
+                                          outdir,
+                                          extinction_probs_mode=extinction_probs_mode,
+                                          possibly_extinct_list=possibly_extinct_list,
+                                          species_specific_regression=species_specific_regression,
+                                          rate_samples=rate_samples,
+                                          n_gen=n_gen,
+                                          burnin=burnin,
+                                          seed=seed,
+                                          load_from_file=load_from_file)
+
+  if(return_object==TRUE){
+    return(py_get_attr(transition_rates_obj,'_simulation_input_data'))
+  }else{
+    return(py_get_attr(transition_rates_obj,'_simdata_outfile'))
+  }
+}
+
 #' Simulate future extinctions
 #' @export
 #' @import reticulate
@@ -17,7 +64,7 @@ run_future_sim = function(simulation_input_data,
                           plot_status_piechart=TRUE,
                           seed=NULL,
                           load_from_file=FALSE
-                          ){
+){
 
   # source python function
   reticulate::source_python("https://raw.githubusercontent.com/tobiashofmann88/iucn_extinction_simulator/master/iucn_sim/iucn_sim.py")
